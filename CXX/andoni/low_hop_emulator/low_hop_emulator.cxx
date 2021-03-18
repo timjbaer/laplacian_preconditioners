bf17b44130a81b83cff02c41541cbea8260d971a
Tim Baer <tbaer2@illinois.edu>
2021-03-18 08:50:50 -0500

    fix subemulator bug on multiple processes

--- CXX/andoni/low_hop_emulator/low_hop_emulator.cxx
+++ CXX/andoni/low_hop_emulator/low_hop_emulator.cxx
@@ -4,7 +4,13 @@ Subemulator::Subemulator(Matrix<REAL> * A, int b) { // A is on (min, +)
   Timer t_subemulator("subemulator");
   t_subemulator.start();
   assert(A->is_sparse); // not strictly necessary, but much more efficient
-  B = ball_matmat(A, b);
+  // B = ball_matmat(A, b);
+  B = new Matrix<REAL>(A->nrow, A->ncol, A->symm|(A->is_sparse*SP), *A->wrld, *A->sr);
+  Vector<bvector<BALL_SIZE>> * ball = ball_bvector<BALL_SIZE>(A, 0, 0);
+  bvec_to_mat(B, ball);
+  delete ball;
+  assert(B->is_sparse); // not strictly necessary, but much more efficient
+  assert(B->topo->order == 2 || B->wrld->np < 4); // A distributed on 2D processor grid (not strictly necessary but scales better)
   Vector<int> * S = samples(A, b);
   connects(A, S, b);
   t_subemulator.stop();
@@ -16,7 +22,7 @@ Subemulator::~Subemulator() {
   delete B;
 }
 
-Vector<int> * Subemulator::samples(Matrix<REAL> * A, int b) { // FIXME: vertex 0 is never selected
+Vector<int> * Subemulator::samples(Matrix<REAL> * A, int b) {
   Timer t_samples("samples");
   t_samples.start();
   int n = A->nrow;
@@ -34,8 +40,9 @@ Vector<int> * Subemulator::samples(Matrix<REAL> * A, int b) { // FIXME: vertex 0
   // if (w->rank == 0) printf("is_close\n");
   // is_close->print();
   (*S)["i"] += Function<int,int>([](int id){ return id ^ 1; })((*is_close)["i"]);
-  (*S)["i"] = (*S)["i"] * ID["i"];
+  (*S)["i"] = (*S)["i"] * ID["i"]; // FIXME: key-value pair (0,0) is always written but since value 0 is addid, vertex 0 is never selected as a leader
   S->sparsify(); // TODO: sparsify earlier?
+  // if (w->rank == 0) printf("S\n");
   // S->print();
   delete is_close;
   t_samples.stop();
@@ -63,18 +70,18 @@ void Subemulator::connects(Matrix<REAL> * A, Vector<int> * S, int b) {
   // q->print();
 
   // Matrix<REAL> * C = new Matrix<REAL>(n, n, A->symm|(A->is_sparse*SP), *w, MIN_PLUS_SR);
-  Matrix<REAL> * C = new Matrix<REAL>(n, n, A->symm|(A->is_sparse*SP), *w, MAX_MONOID); // MAX_MONOID is a hack to avoid Transform accumulating
-  (*C)["ij"] = (*A)["ij"];
   // std::function<REAL(bpair,REAL,bpair)> f = [](bpair x, REAL a, bpair y){ return x.dist + a + y.dist; };
   // Tensor<bpair> * vec_list[2] = {q, q};
   // Multilinear<REAL,bpair,REAL>(A, vec_list, C, f); // e = (q_i,q_j) for (i,j) \in E
   // Multilinear<REAL,bpair,REAL>(B, vec_list, C, f); // e = (q_i,q_j) for j \in B_i
+  Matrix<REAL> * C = new Matrix<REAL>(n, n, A->symm|(A->is_sparse*SP), *w, MAX_MONOID); // MAX_MONOID is a hack to avoid Transform accumulating
+  (*C)["ij"] = (*A)["ij"];
   Transform<bpair,REAL>([](bpair pr, REAL & c){ c += pr.dist; })((*q)["i"], (*C)["ij"]);
   Transform<bpair,REAL>([](bpair pr, REAL & c){ c += pr.dist; })((*q)["j"], (*C)["ij"]);
   // if (w->rank == 0) printf("C:\n");
   // C->print_matrix();
   Vector<int> * q_vertices = new Vector<int>(n, *w);
-  (*q_vertices)["i"] = Function<bpair,int>([](bpair pair){ return pair.vertex; })((*q)["i"]);
+  (*q_vertices)["i"] = Function<bpair,int>([](bpair pair){ return std::max(pair.vertex, 0); })((*q)["i"]); // FIXME: if vertex 0 is a leader, PTAP is not correct
   H = PTAP<REAL>(C, q_vertices);
   delete q_vertices;
   delete C;
