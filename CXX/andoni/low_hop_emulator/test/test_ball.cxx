976748424d2d0e0824264057477427d4edfd495d
meng-sun <ms65@illinois.edu>
2022-04-29 02:13:40 -0400

    [doc] add documentation to test_ball
    some reordering of code
    asterisks are reminders for potential changes to be made

--- CXX/andoni/low_hop_emulator/test/test_ball.cxx
+++ CXX/andoni/low_hop_emulator/test/test_ball.cxx
@@ -1,7 +1,56 @@
 #include "test.h"
 #include "../ball.h"
 
-Matrix<REAL> * correct_ball(Matrix<REAL> * A, int b) { // assumes correct of filter()
+//===================================================================
+// Test Ball
+//===================================================================
+// Input: graph
+// Output: number of incorrect closest neighbors
+//
+// This tests the b-closest neighbor functions that use
+// 1. matrix format only
+//   a. path doubling (ball_matmat)
+// 2. matrix-vector format
+//   b. single ball update (ball_bvector)
+//   c. multilinear (ball_multilinear)
+// and compares the b closest neighbors derived from these against
+// the true b closest neighbors for all vertices
+//
+// ------------------------------------------------------------------
+//
+// To run:
+//
+// mpirun -n 4 ./test_ball <graph flags>
+//
+// optional flags are:
+// -critter_mode                      --> disabled
+//
+// -b [Z]                             number of closest neighbors
+//
+// -bvec | -multi                    run bvector or multilinear
+//
+// -d [1,2]                           1D or 2D data partition layout
+//
+// -conv                              detects early convergence
+//
+// -square                            early update of A that changes
+//                                     B(i+1,u) = min+_k(B(i,u), A_kv)
+//                                     into
+//                                     B(i+1,u)=min+_k(B(i,u), B(i,k))
+//                                     or min+_k(B(i,u),B(i+1,k))
+//                                     depending on order
+//
+//===================================================================
+
+
+/* For a graph G(V,E), creates a 1D layout symmetric matrix
+ * that for each row r contains the b closest neighbors of vertex
+ * r
+ *
+ * we compute b closest neighbors using log2(|V|) iterations
+ * of path doubling and then filtering each row to have the closest b
+ */
+Matrix<REAL> * correct_ball(Matrix<REAL> * A, int b) {
   int np = A->wrld->np;
   int plens[1] = { np };
   Partition part(1, plens);
@@ -12,10 +61,16 @@ Matrix<REAL> * correct_ball(Matrix<REAL> * A, int b) { // assumes correct of fil
   for (int i = 0; i < log2(B->nrow); ++i) {
     (*B)["ij"] += (*B)["ik"] * (*B)["kj"];
   }
-  filter(B, b);
+  filter(B, b); // assumes correct filter()
   return B;
 }
 
+//-------------------------------------------------------------------
+
+/* if B is not matrix, then converts vector of balls for each
+ * vertex B to matrix B and then checks each element between
+ * A and B, returns 1 if different else 0
+ */
 int64_t check_ball(Matrix<REAL> * A, Matrix<REAL> * B, int b) {
   Matrix<REAL> * correct = correct_ball(A, b);
   // if (A->wrld->rank == 0)
@@ -35,31 +90,80 @@ int64_t check_ball(Matrix<REAL> * A, Vector<bvector<BALL_SIZE>> * B, int b) {
   return s;
 }
 
+//-------------------------------------------------------------------
+
 int main(int argc, char** argv)
 {
   int const in_num = argc;
   char** input_str = argv;
-  int critter_mode=0;
-  int b=BALL_SIZE;
+
+  // flags
+  int critter_mode=0; // disabled
+  int b=BALL_SIZE; // *
   int bvec=0;
   int multi=0;
   int d=2;
   int conv=0;
   int square=0;
 
+  // MPI
   int rank;
   int np;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
   {
+    // CTF
     World w(argc, argv);
-    Matrix<REAL> * B = get_graph(argc, argv, w);
+
+    // parse flags --------------------------------------------------
+
     if (getCmdOption(input_str, input_str+in_num, "-d")){
       d = atoi(getCmdOption(input_str, input_str+in_num, "-d"));
       if (d < 1 || d > 2) d = 2;
     }
-    Matrix<REAL> * A;
+
+    if (getCmdOption(input_str, input_str+in_num, "-b")){
+      b = atoi(getCmdOption(input_str, input_str+in_num, "-b"));
+      if (b < 0) b = 0;
+    }
+
+    if (getCmdOption(input_str, input_str+in_num, "-bvec")){
+      bvec = atoi(getCmdOption(input_str, input_str+in_num, "-bvec"));
+      if (bvec < 0) bvec = 0;
+    } else bvec = 0;
+
+    if (getCmdOption(input_str, input_str+in_num, "-multi")){
+      multi = atoi(getCmdOption(input_str, input_str+in_num, "-multi"));
+      if (multi < 0) multi = 0;
+    } else multi = 0;
+
+    assert(!multi || !bvec);
+
+    if (getCmdOption(input_str, input_str+in_num, "-conv")){
+      conv = atoi(getCmdOption(input_str, input_str+in_num, "-conv"));
+      if (conv < 0) conv = 0;
+    } else conv = 0;
+
+    if (getCmdOption(input_str, input_str+in_num, "-square")){
+      square = atoi(getCmdOption(input_str, input_str+in_num, "-square"));
+      if (square < 0) square = 0;
+    } else square = 0;
+
+    if (getCmdOption(input_str, input_str+in_num, "-critter_mode")){
+      critter_mode = atoi(getCmdOption(input_str, input_str+in_num, "-critter_mode"));
+      if (critter_mode < 0) critter_mode = 0;
+    } else critter_mode = 0;
+
+#ifdef CRITTER
+      critter::start(critter_mode);
+#endif
+
+    // create ball computation matrix from input graph  -------------
+
+    Matrix<REAL> * B = get_graph(argc, argv, w); // temp input graph
+    Matrix<REAL> * A; // ball computation matrix
+
     if (d == 1) { // 1D distribution (block along rows)
       int plens[1] = { np };
       Partition part(1, plens);
@@ -68,67 +172,59 @@ int main(int argc, char** argv)
     } else { // default (2D) distribution
       A = new Matrix<REAL>(B->nrow, B->ncol, B->symm|(B->is_sparse*SP), w, MIN_PLUS_SR);
     }
+
     (*A)["ij"] = (*B)["ij"]; // change to (min, +) semiring and correct distribution
     assert(A->is_sparse); // not strictly necessary, but much more efficient
+
     delete B;
-    if (getCmdOption(input_str, input_str+in_num, "-b")){
-      b = atoi(getCmdOption(input_str, input_str+in_num, "-b"));
-      if (b < 0) b = 0;
-    }
-    if (getCmdOption(input_str, input_str+in_num, "-bvec")){
-      bvec = atoi(getCmdOption(input_str, input_str+in_num, "-bvec"));
-      if (bvec < 0) bvec = 0;
-    } else bvec = 0;
-    if (getCmdOption(input_str, input_str+in_num, "-multi")){
-      multi = atoi(getCmdOption(input_str, input_str+in_num, "-multi"));
-      if (multi < 0) multi = 0;
-    } else multi = 0;
-    assert(!multi || !bvec);
-    if (getCmdOption(input_str, input_str+in_num, "-conv")){
-      conv = atoi(getCmdOption(input_str, input_str+in_num, "-conv"));
-      if (conv < 0) conv = 0;
-    } else conv = 0;
-    if (getCmdOption(input_str, input_str+in_num, "-square")){
-      square = atoi(getCmdOption(input_str, input_str+in_num, "-square"));
-      if (square < 0) square = 0;
-    } else square = 0;
-    if (getCmdOption(input_str, input_str+in_num, "-critter_mode")){
-      critter_mode = atoi(getCmdOption(input_str, input_str+in_num, "-critter_mode"));
-      if (critter_mode < 0) critter_mode = 0;
-    } else critter_mode = 0;
-#ifdef CRITTER
-      critter::start(critter_mode);
-#endif
+
+    // run test -----------------------------------------------------
+    // spacing *
+
       if (A != NULL) {
-        if (b > A->nrow) b = A->nrow;
-        perturb(A);
+        if (b > A->nrow) b = A->nrow; // *
+
+        perturb(A); // for each existing edge add rand len btwn [0,1]
+
 #ifdef DEBUG
-        if (w.rank == 0)
-          printf("A:\n");
+        if (w.rank == 0) printf("A:\n");
         A->print_matrix();
 #endif
-        if (!b)
-          b = ceil(log2(A->nrow));
+
+        if (!b) b = ceil(log2(A->nrow)); // *
+
+        // compute b-closest neighbors of all vertices
+        // by path doubling using matrix format
         if (!bvec && !multi) {
           TAU_FSTART(ball via matmat);
           double stime = MPI_Wtime();
           Matrix<REAL> * ball = ball_matmat(A, b);
           double etime = MPI_Wtime();
           TAU_FSTOP(ball via matmat);
+
 #ifdef DEBUG
-          if (w.rank == 0)
-            printf("ball (via matmat):\n");
-          ball->print_matrix();
+          if (w.rank == 0) printf("ball (via matmat):\n");
+          ball->print_matrix(); // *
 #endif
+
 #if defined TEST || defined DEBUG
+          // compare results from b-closest neighbor function
+          // to true b-closest neighbors
           int64_t diff = check_ball(A, ball, b);
           if (w.rank == 0)
-            printf("ball (via matmat) diff: %" PRId64 "\n", diff);
+            printf("ball (via matvec) diff: %" PRId64 "\n", diff);
 #endif
+
           delete ball;
+
           if (w.rank == 0)
-            printf("ball (via matmat) done in %1.2lf\n", etime - stime);
+            printf("ball (via matvec) done in %1.2lf\n", etime - stime);
+
+
         } else {
+        // compute b-closest neighbors of all using vector
+        // methods such as single ball update or using
+        // multilinear format
           TAU_FSTART(ball via matvec);
           double stime = MPI_Wtime();
           Vector<bvector<BALL_SIZE>> * ball = nullptr;
@@ -138,26 +234,34 @@ int main(int argc, char** argv)
             ball = ball_multilinear<BALL_SIZE>(A,conv,square);
           double etime = MPI_Wtime();
           TAU_FSTOP(ball via matvec);
+
 #ifdef DEBUG
-          if (w.rank == 0)
-            printf("ball (via matvec):\n");
-          ball->print();
+          if (w.rank == 0) printf("ball (via matvec):\n");
+          ball->print(); // *
 #endif
+
 #if defined TEST || defined DEBUG
+          // compare results from b-closest neighbor function
+          // to true b-closest neighbors
           int64_t diff = check_ball(A, ball, b);
           if (w.rank == 0)
             printf("ball (via matvec) diff: %" PRId64 "\n", diff);
 #endif
+
           delete ball;
+
           if (w.rank == 0)
             printf("ball (via matvec) done in %1.2lf\n", etime - stime);
         }
+
       }
+
 #ifdef CRITTER
       critter::stop(critter_mode);
 #endif
     delete A;
   }
+
   MPI_Finalize();
   return 0;
 }
